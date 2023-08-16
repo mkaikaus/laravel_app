13dabbf55529d07f69e680fd593476ce3f0b870d
mkaikaus30 <mkaikaus30@gmail.com>
2023-08-17 00:45:49 +0600

    fix paginator UI

--- blog/app/Providers/AppServiceProvider.php
+++ blog/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,8 +19,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
+     
     public function boot()
     {
+        Paginator::useBootstrap();
         Schema::defaultStringLength(191);
     }
 }
