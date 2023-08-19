1ea29991833ede465de97c10826482345336efb1
mkaikaus30 <mkaikaus30@gmail.com>
2023-08-19 23:16:49 +0600

    fix update form

--- blog/resources/views/posts/edit.blade.php
+++ blog/resources/views/posts/edit.blade.php
@@ -8,10 +8,7 @@
             @csrf
             @method('PUT')
             <div class="mb-4">
-                <!-- <label for="body" class="sr-only"></label> -->
-                <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-2 rounded-lg text-left @error('body') border-red-500 @enderror">
-                {{ old('body', $post->body) }}</textarea>
-
+                <textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-2 rounded-lg text-left @error('body') border-red-500 @enderror">{{ old('body', $post->body) }}</textarea>
                 @error('body')
                 <div class="text-red-500 mt-2 text-sm">
                     {{ $message }}
